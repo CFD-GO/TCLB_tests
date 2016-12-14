6a0a1dccc5b5a3426b84919b20e2c6b295b872c2
mdzik <dziki01@gmail.com>
2016-12-14 16:06:27 +0100

    Updated to mach PR #117 fixes

--- d2q9_kuper/runexternal.py
+++ d2q9_kuper/runexternal.py
@@ -5,4 +5,4 @@ def test(*args):
 
     f0 = np.asarray(args[2])
 
-    np.savetxt('output/drop_sumF.csv', np.array([np.sum(f0,axis=1),np.average(f0,axis=1)]))
+    np.savetxt('output/stats.csv', np.array([np.min(f0), np.max(f0)]).T, delimiter=",")

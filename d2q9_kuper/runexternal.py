c2df21a83b5badc4f166473aa27dda081f33a256
L Laniewski-Wollk <uqllanie@uq.edu.au>
2020-01-14 15:48:59 +1000

    Correcting error in d2q9_kuper drop test

--- d2q9_kuper/runexternal.py
+++ d2q9_kuper/runexternal.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def test(*args):
 
-    f0 = np.asarray(args[2])
+    extra = args[0]
+    f0 = np.asarray(args[extra+0])
 
     np.savetxt('output/stats.csv', np.array([np.min(f0), np.max(f0)]).T, delimiter=",")
 
